26a1c98f0ecaba73bfd22728f1045f8a3be04063
Diego Nevado <90259282+diego-nevado@users.noreply.github.com>
2023-04-26 16:53:37 +0200

    first commit

---
+++ 00_hello.py
@@ -0,0 +1,25 @@
+# Esto es un comentario: Hola mundo
+# Nuestro Hola Mundo en Python
+print("Hola mundo")
+print('Hola mundo')
+
+
+""" Este es un 
+comentario
+en varias lineas """
+
+
+'''
+Este tambien es un 
+comentario
+en varias lineas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(3 + 1j))  # Tipo 'complex'
+print(type(True))  # Tipo 'bool'
+
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
